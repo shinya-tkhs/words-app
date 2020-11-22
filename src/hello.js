67a175f7c39856d83fbdc169605e2513b094fece
shinya.takahashi <shinya.takahashi@x-factory.jp>
2020-11-22 23:47:41 +0900

    add lang api

--- src/hello.js
+++ src/hello.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+const { Translate } = require('@google-cloud/translate').v2;
+const translateClient = new Translate();
 
 app.use(express.static(__dirname + '/public'));
 
@@ -17,4 +19,18 @@ app.get('/hello', async (req, res) => {
 app.get('/bye', async (req, res) => {
   const word = req.query.b;
   res.send(`Bye, you enetered ${word}`);
+});
+
+app.get('/translate/:lang', async (req, res) => {
+  try {
+    const word = req.query.word;
+    console.log(word);
+    const result = await translateClient.translate(
+      word,
+      req.params.lang
+    )
+    res.send(`${result[0]}\n\n`);
+  } catch (err) {
+    console.error(err);
+  }
 });
